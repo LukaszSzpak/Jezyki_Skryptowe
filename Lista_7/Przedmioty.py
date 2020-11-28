37dff00d62b2149f86614cc44220ad0c33ae31da
Łukasz Szpak <alukasz1@interia.pl>
2020-11-28 21:38:05 +0100

    Fix

--- Lista_7/Przedmioty.py
+++ Lista_7/Przedmioty.py
@@ -2,16 +2,16 @@ from LevSim import calc_lev_sim
 
 class Przedmioty:
     def __init__(self):
-        self.subject_dict = {}
+        self.__subject_dict = {}
 
     def get_subject_code(self, subject_name):
-        if not self.subject_dict:
+        if not self.__subject_dict:
             return None
 
         temp_list = []
         act_div = 0
         while not temp_list:
-            for code, name in self.subject_dict.items():
+            for code, name in self.__subject_dict.items():
                 if calc_lev_sim(name, subject_name) <= act_div:
                     temp_list.append(code)
                 act_div += 1
@@ -21,16 +21,16 @@ class Przedmioty:
         return temp_list[0]
 
     def add_subject(self, subject_code, subject_name):
-        if subject_code in self.subject_dict.keys() or subject_name in self.subject_dict.values():
+        if subject_code in self.__subject_dict.keys() or subject_name in self.__subject_dict.values():
             return -1
 
-        self.subject_dict[subject_code] = subject_name
-        return len(self.subject_dict.keys())
+        self.__subject_dict[subject_code] = subject_name
+        return len(self.__subject_dict.keys())
 
     def get_subject(self, subject_code):
-        if subject_code in self.subject_dict.keys():
-            return self.subject_dict[subject_code]
+        if subject_code in self.__subject_dict.keys():
+            return self.__subject_dict[subject_code]
         return None
 
     def __str__(self):
-        return str(self.subject_dict)
+        return str(self.__subject_dict)
