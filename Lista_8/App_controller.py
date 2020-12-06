34f6c068258dd202994b0916bb319a4dae6591dc
Łukasz Szpak <alukasz1@interia.pl>
2020-12-06 14:49:36 +0100

    Add app controller

--- Lista_8/App_controller.py
+++ Lista_8/App_controller.py
@@ -4,22 +4,29 @@ from Covid_serv import Covid_service
 class App_controller:
     def __init__(self):
         self.covid_service = Covid_service()
+        self.covid_service.make_data()
 
     def deaths_world(self, date=None):
-        pass
+        if date is None:
+            return self.covid_service.get_world_sum()[1]
 
     def cases_world(self, date=None):
-        pass
+        if date is None:
+            return self.covid_service.get_world_sum()[0]
 
     def deaths_continent(self, continent_name, date=None):
-        pass
+        if date is None:
+            return self.covid_service.get_continent_deaths(continent_name)
 
     def cases_continent(self, continent_name, date=None):
-        pass
+        if date is None:
+            return self.covid_service.get_continent_cases(continent_name)
 
     def deaths_country(self, country_name, date=None):
-        pass
+        if date is None:
+            return self.covid_service.get_country_deaths(country_name)
 
     def cases_country(self, country_name, date=None):
-        pass
+        if date is None:
+            return self.covid_service.get_country_cases(country_name)
 
