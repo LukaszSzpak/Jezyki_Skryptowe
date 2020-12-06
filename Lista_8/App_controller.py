4ccc8730d3d5041f6ab863d71c7b82fb31667d34
Łukasz Szpak <alukasz1@interia.pl>
2020-12-06 23:44:04 +0100

    Change dict to private

--- Lista_8/App_controller.py
+++ Lista_8/App_controller.py
@@ -5,61 +5,63 @@ from itertools import islice
 
 class App_controller:
     def __init__(self):
-        self.covid_service = Covid_service()
-        self.covid_service.make_data()
+        self.__covid_service = Covid_service()
+        self.__covid_service.make_data()
 
     def deaths_world(self, date_day=None, date_month=None):
         if date_day is None and date_month is None:
-            return self.covid_service.get_world_sum()[1]
+            return self.__covid_service.get_world_sum()[1]
         if date_day is None:
-            return self.covid_service.get_world_month(date_month)[1]
-        return self.covid_service.get_world_day(date_day, date_month)[1]
+            return self.__covid_service.get_world_month(date_month)[1]
+        return self.__covid_service.get_world_day(date_day, date_month)[1]
 
     def cases_world(self, date_day=None, date_month=None):
         if date_day is None and date_month is None:
-            return self.covid_service.get_world_sum()[0]
+            return self.__covid_service.get_world_sum()[0]
         if date_day is None:
-            return self.covid_service.get_world_month(date_month)[0]
-        return self.covid_service.get_world_day(date_day, date_month)[0]
+            return self.__covid_service.get_world_month(date_month)[0]
+        return self.__covid_service.get_world_day(date_day, date_month)[0]
 
     def deaths_continent(self, continent_name, date_day=None, date_month=None):
         if date_day is None and date_month is None:
-            return self.covid_service.get_continent_deaths(continent_name)
+            return self.__covid_service.get_continent_deaths(continent_name)
         if date_day is None:
-            return self.covid_service.get_continent_month_deaths(continent_name, month_name=date_month)
-        return self.covid_service.get_continent_day_deaths(continent_name, day=date_day, month_name=date_month)
+            return self.__covid_service.get_continent_month_deaths(continent_name, month_name=date_month)
+        return self.__covid_service.get_continent_day_deaths(continent_name, day=date_day, month_name=date_month)
 
     def cases_continent(self, continent_name, date_day=None, date_month=None):
         if date_day is None and date_month is None:
-            return self.covid_service.get_continent_cases(continent_name)
+            return self.__covid_service.get_continent_cases(continent_name)
         if date_day is None:
-            return self.covid_service.get_continent_month_cases(continent_name, month_name=date_month)
-        return self.covid_service.get_continent_day_cases(continent_name, day=date_day, month_name=date_month)
+            return self.__covid_service.get_continent_month_cases(continent_name, month_name=date_month)
+        return self.__covid_service.get_continent_day_cases(continent_name, day=date_day, month_name=date_month)
 
     def deaths_country(self, country_name, date_day=None, date_month=None):
         if date_day is None and date_month is None:
-            return self.covid_service.get_country_deaths(country_name)
+            return self.__covid_service.get_country_deaths(country_name)
         if date_day is None:
-            return self.covid_service.get_country_month_deaths(country_name, month_name=date_month)
-        return self.covid_service.get_country_day_deaths(country_name, day=date_day, month_name=date_month)
+            return self.__covid_service.get_country_month_deaths(country_name, month_name=date_month)
+        return self.__covid_service.get_country_day_deaths(country_name, day=date_day, month_name=date_month)
 
     def cases_country(self, country_name, date_day=None, date_month=None):
         if date_day is None and date_month is None:
-            return self.covid_service.get_country_cases(country_name)
+            return self.__covid_service.get_country_cases(country_name)
         if date_day is None:
-            return self.covid_service.get_country_month_cases(country_name, month_name=date_month)
-        return self.covid_service.get_country_day_cases(country_name, day=date_day, month_name=date_month)
+            return self.__covid_service.get_country_month_cases(country_name, month_name=date_month)
+        return self.__covid_service.get_country_day_cases(country_name, day=date_day, month_name=date_month)
 
     def check_location_and_get_deaths(self, location, date_day=None, date_month=None):
         try:
             if location == 'swiat':
-                return self.deaths_world(date_day=date_day, date_month=date_month)
+                return 'Zgony: {0}'.format(self.deaths_world(date_day=date_day, date_month=date_month))
 
-            if self.covid_service.is_it_country(location):
-                return self.deaths_country(country_name=location, date_month=date_month, date_day=date_day)
+            if self.__covid_service.is_it_country(location):
+                return 'Zgony: {0}'.format(self.deaths_country(country_name=location, date_month=date_month,
+                                                               date_day=date_day))
 
-            if self.covid_service.is_it_continent(location):
-                return self.deaths_continent(continent_name=location, date_month=date_month, date_day=date_day)
+            if self.__covid_service.is_it_continent(location):
+                return 'Zgony: {0}'.format(self.deaths_continent(continent_name=location, date_month=date_month,
+                                                                 date_day=date_day))
 
             return 'Bledna lokalizacja !'
         except:
@@ -68,24 +70,26 @@ class App_controller:
     def check_location_and_get_cases(self, location, date_day=None, date_month=None):
         try:
             if date_month is not None:
-                if not self.covid_service.is_it_month(date_month):
+                if not self.__covid_service.is_it_month(date_month):
                     return 'Bledny miesiac !'
 
             if location == 'swiat':
-                return self.cases_world(date_day=date_day, date_month=date_month)
+                return 'Zachorowania: {0}'.format(self.cases_world(date_day=date_day, date_month=date_month))
 
-            if self.covid_service.is_it_country(location):
-                return self.cases_country(country_name=location, date_month=date_month, date_day=date_day)
+            if self.__covid_service.is_it_country(location):
+                return 'Zachorowania: {0}'.format(self.cases_country(country_name=location, date_month=date_month,
+                                                                     date_day=date_day))
 
-            if self.covid_service.is_it_continent(location):
-                return self.cases_continent(continent_name=location, date_month=date_month, date_day=date_day)
+            if self.__covid_service.is_it_continent(location):
+                return 'Zachorowania: {0}'.format(self.cases_continent(continent_name=location, date_month=date_month,
+                                                                       date_day=date_day))
 
             return 'Bledna lokalizacja !'
         except:
             return 'Data niedostępna !'
 
     def get_max_cases(self, number_of_days):
-        get_dict = self.covid_service.get_world_dates()
+        get_dict = self.__covid_service.get_world_dates()
         sorted_dict = sorted(get_dict.items(), key=lambda day: day[1].get_cases(), reverse=True)
 
         str = ''
@@ -94,7 +98,7 @@ class App_controller:
         return str
 
     def get_max_deaths(self, number_of_days):
-        get_dict = self.covid_service.get_world_dates()
+        get_dict = self.__covid_service.get_world_dates()
         sorted_dict = sorted(get_dict.items(), key=lambda day: day[1].get_deaths(), reverse=True)
 
         str = ''
